5b910d4e45816a1400fbcb8b274e2ea3f97d4ce2
Tobias Perelstein <tobias.perelstein@gmail.com>
2014-05-01 21:57:00 -0400

    redoing free

--- mallocMeCapn.c
+++ mallocMeCapn.c
@@ -1,7 +1,6 @@
-
 #define malloc(x) mymalloc(x, __FILE__, __LINE__)
 #define free(x) myfree(x, __FILE__, __LINE__)
-
+//insert macro for line # here put macros in header
 #define BLOCKSIZE 5000;
 static char bigBlock[BLOCKSIZE];
 
@@ -13,40 +12,44 @@ struct mementry
     int size;
 };
 
+struct mementry *root = 0;
+
 void *mymalloc(int size)
-{ static int initialized = 0;   //initializing bigblock static struct mementry *root; struct mementry *p, *succ;
+{
+    //add print statements.
+    static int initialized = 0;                                                       //initializing bigblock static struct mementry *root; struct mementry *p, *succ;
     if (!initialized)
     {
         root = (struct mementry *) bigBlock;
-        root->prev = root->succ = 0;        //making prevoius and successors null
-        root->size = BLOCKSIZE - sizeof(struct mementry);   //size is blocksize minus mementry size
-        root->isFree = 1;   //flagging this to be freed.
-        initialized = 1;    //space has been initialized
+        root->prev = root->succ = 0;                                                //making prevoius and successors null
+        root->size = BLOCKSIZE - sizeof(struct mementry);                           //size is blocksize minus mementry size
+        root->isFree = 1;                                                           //flagging this to be freed.
+        initialized = 1;                                                            //space has been initialized
     }
-    p = root;             //pointing to the root
+    p = root;                                                                       //pointing to the root
     do
     {
-        if (p->size < size)         //Is it too small?
-            p = p->succ;              //increment pointer to its next to find free space
-        else if (p->isFree == 0)    //Is it in use?
-            p = p->succ;              //keep moving it since it's not free
-        else if (p->size < (size + sizeof(struct mementry)))    //cannot insert new mem entry
+        if (p->size < size)                                                         //Is it too small?
+            p = p->succ;                                                            //increment pointer to its next to find free space
+        else if (p->isFree == 0)                                                    //Is it in use?
+            p = p->succ;                                                            //keep moving it since it's not free
+        else if (p->size < (size + sizeof(struct mementry)))                        //cannot insert new mem entry
         {
-            p->isFree = 0;            //too small to chop up
+            p->isFree = 0;                                                          //too small to chop up
             return (char *) p + sizeof(struct mementry);
         }
-        //big enough to chop up and return requested amount
-        else  //Other blocks appended
+                                                                                    //big enough to chop up and return requested amount
+        else                                                                        //Other blocks appended
         {
-            //placing this  mementry struct passed p + the size of the struct p is pointing to + the size passed in
+                                                                                    //placing this  mementry struct passed p + the size of the struct p is pointing to + the size passed in
             succ = (struct mementry*) ((char*)p + sizeof(struct mementry) + size);
             succ->prev = p;
             succ->succ = p->succ;
-            if (p->succ != 0)               //if p's  successor is not null
-                p->succ->prev = succ;       //p's successors prevoius is now the successor
+            if (p->succ != 0)                                                      //if p's  successor is not null
+                p->succ->prev = succ;                                              //p's successors prevoius is now the successor
             p->succ = succ;
-            succ->size = p->size - sizeof(struct mementry) - size;    //the successor takes up the size of p->size less the struct size minus the size passed in
-            succ->isFree = 1;             //this
+            succ->size = p->size - sizeof(struct mementry) - size;                 //the successor takes up the size of p->size less the struct size minus the size passed in
+            succ->isFree = 1;                                                      //this
             p->size = size;
             p->isFree = 0;
             return (char*)p + sizeof(struct mementry);
@@ -59,7 +62,24 @@ void *mymalloc(int size)
 
 void myfree(void * p)
 {
-    struct mementry *ptr, *pred, *succ;
+    //Need to show that this is bad memory
+    //check if free is marked 0, is p = mempt+sizeof(mp)
+    //if not calling it on valid pointer, call on
+    //need to iterate through all mementry structs
+    //to find good pointers for freeing
+    //iterate through linked list
+    struct mementry *ptr = root;
+    for(ptr = root; ptr !=NULL; ptr=ptr->succ){
+        //check if p is within this range
+        start (char*)ptr +sizeof(mementry)
+        end = start+ptr->size
+        //if equal to end or between end, error
+        //if we get to end of loop and haven't found pointer, error.
+        //check for double free
+        //
+    }
+
+
 
     ptr = (struct mementry*)((char*)p - sizeof(struct mementry));
 
@@ -68,34 +88,6 @@ void myfree(void * p)
         printf("That was freed before. (Double Free)!\n");
         return;
     }
-    if ((pred = ptr->prev) != 0 && pred->isFree)
-    {
-        pred->size += sizeof(struct mementry) + ptr->size;  //Merge with predecessor
-        pred->succ = ptr->succ;
-        if (ptr->succ != 0)
-            ptr->succ->prev = pred;
-
-        printf("Freeing block %#x merging with predecessor new size is %d.\n", p, pred->size);
-    }
-    else
-    {
-        printf("Freeing block %#x.\n", p);
-        ptr->isFree = 1;
-        if (ptr->succ != 0)
-            ptr->succ->prev = pred;
-    }
-    if ((succ = ptr-sdf>succ) != 0 && succ->isFree)
-    {
-        printf("pred: %d\n", ptr->isFree);
-        pred = ptr;//pred needs to be assigned a value!!!
-        pred->size += sizeof(struct mementry) + succ->size;  //Merge with successor
-        pred->succ = succ->succ;
-        if (succ->succ != 0)
-            succ->succ->prev = pred;
-
-        printf("Freeing block %#x merging with successor new size is %d.\n", p, pred->size);
-    }
-    return;
 }
 
 int main()
